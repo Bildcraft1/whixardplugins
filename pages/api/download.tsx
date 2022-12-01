279400ea8ac5d83fb2b4a06189a5bf9dcbc22c60
WhiXard <bildcraft10000@gmail.com>
2022-12-01 12:26:20 +0100

    Another patch

--- pages/api/download.tsx
+++ pages/api/download.tsx
@@ -1,6 +1,6 @@
 import type { NextRequest } from 'next/server'
 
-export default function handler(req: NextRequest, res) {
+export default function handler(req, res) {
     const { pid } = req.query
     res.end(`Post: ${pid}`)
 }
