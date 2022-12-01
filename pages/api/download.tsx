1ef5777d35fb1c8f0d7628705e86717c9f46aa20
WhiXard <bildcraft10000@gmail.com>
2022-12-01 09:56:21 +0100

    Experimenting with NextJS apis

--- pages/api/download.tsx
+++ pages/api/download.tsx
@@ -1,16 +1,11 @@
-// Next.js Edge API Routes: https://nextjs.org/docs/api-routes/edge-api-routes
-
 import type { NextRequest } from 'next/server'
 
 export const config = {
-    runtime: 'experimental-edge',
+  runtime: 'experimental-edge',
 }
 
-export default async function (req: NextRequest) {
-    return new Response(JSON.stringify({ name: 'John Doe' }), { status: 200, headers: { 'Content-Type': 'application/json' } })
-}
-
-export default function handler(req, res) {
-    const { pid } = req.query
-    res.end(`Post: ${pid}`)
+export default async function handler(req: NextRequest) {
+  const { searchParams } = new URL(req.url)
+  const email = searchParams.get('email')
+  return new Response(email)
 }
