d6b8d725e9f2abe2feb2484eafdeda65e9d74acd
WhiXard <bildcraft10000@gmail.com>
2022-12-01 10:00:46 +0100

    Test

--- pages/api/download.tsx
+++ pages/api/download.tsx
@@ -1,11 +1,10 @@
 import type { NextRequest } from 'next/server'
 
 export const config = {
-  runtime: 'experimental-edge',
+    runtime: 'experimental-edge',
 }
 
-export default async function handler(req: NextRequest) {
-  const { searchParams } = new URL(req.url)
-  const email = searchParams.get('email')
-  return new Response(email)
+export default async function handler(req: NextRequest, res: NextRequest) {
+    const { pid } = req.query
+    res.end(`Post: ${pid}`)
 }
